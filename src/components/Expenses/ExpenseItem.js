21c47f72359c7ac25b9a76c9236447b043f08e15
manishkarki358 <rajkarki358358@gmail.com>
2023-02-15 20:56:30 +0545

    learning state

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+
+function ExpenseItem(props) {
+//STATE
+
+  // useState returns array with two elements so destructuring
+ /*  destructuring such as title refers to current value and setTitle refers
+  to function for updating state or values */
+
+  const [title,setTitle]=useState(props.title)
+  const [amount,setAmount]=useState(props.amount)
+  const clickHandler=()=>{
+    setAmount("bankrupt")
+    setTitle('updated title')
+    console.log("clicked!!!!!!!!!!");
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price' onClick={clickHandler}>${amount}</div>
+      </div>
+     <button onClick={clickHandler}>change title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
